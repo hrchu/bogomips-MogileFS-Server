hachi/MogileFS-Server

commit fa30b6aa44e870251079edcc158388549de57060

Reduce unnecessary sleeping and parent traffic.
The only place that ever told 'every' how often to sleep right was the fsck
worker. Now everyone does it closer to correctly.

Reducing the sleeps also required a minor overhaul of all the parent_ping
calls.

Workers no longer wake up unnecessarily and no should submit far fewer noise
to the parent process, which should help increase throughput overall.

lib/MogileFS/Util.pm
@@ -34,7 +34,6 @@ sub every {
         my $now = Time::HiRes::time();
         my $took = $now - $start;
         my $sleep_for = defined $explicit_sleep ? $explicit_sleep : ($delay - $took);
-        next unless $sleep_for > 0;
 
         # simple case, not in a child process (this never happens currently)
         unless ($psock_fd) {
@@ -42,19 +41,11 @@ sub every {
             next;
         }
 
-        while ($sleep_for > 0) {
-            my $last_time_pre_sleep = $now;
-            $worker->forget_woken_up;
-            if (wait_for_readability($psock_fd, $sleep_for)) {
-                # TODO: uncomment this and watch an idle server and how many wakeups.  could optimize.
-                #local $Mgd::POST_SLEEP_DEBUG = 1;
-                #warn "WOKEN UP FROM SLEEP in $worker [$$]\n";
-                $worker->read_from_parent;
-                next CODERUN if $worker->was_woken_up;
-            }
-            $now = Time::HiRes::time();
-            $sleep_for -= ($now - $last_time_pre_sleep);
-        }
+        Time::HiRes::sleep($sleep_for) if $sleep_for > 0;
+        #local $Mgd::POST_SLEEP_DEBUG = 1;
+        # This calls read_from_parent. Workers used to needlessly call
+        # parent_ping constantly.
+        $worker->parent_ping;
     }
 }
 

lib/MogileFS/Worker.pm
@@ -74,7 +74,7 @@ sub wait_for_monitor {
 sub still_alive {
     my $self = shift;
     my $now = time();
-    if ($now > $self->{last_ping}) {
+    if ($now > $self->{last_ping} + ($self->watchdog_timeout / 4)) {
         $self->send_to_parent(":still_alive");  # a no-op, just for the watchdog
         $self->{last_ping} = $now;
     }
@@ -134,6 +134,7 @@ sub read_from_parent {
     # while things are immediately available,
     # (or optionally sleep a bit)
     while (MogileFS::Util::wait_for_readability(fileno($psock), $timeout)) {
+        $timeout = 0; # only wait on the timeout for the first read.
         my $buf;
         my $rv = sysread($psock, $buf, 1024);
         if (!$rv) {

lib/MogileFS/Worker/Delete.pm
@@ -153,7 +153,6 @@ sub process_deletes2 {
 
     while (my $todo = shift @$queue_todo) {
         $self->still_alive;
-        $self->read_from_parent;
 
         # load all the devids related to this fid, and delete.
         my $fid    = MogileFS::FID->new($todo->{fid});
@@ -270,7 +269,6 @@ sub process_deletes {
         last if ++$done > PER_BATCH;
 
         $self->still_alive;
-        $self->read_from_parent;
         my ($fid, $devid) = @$dm;
         error("deleting fid $fid, on devid ".($devid || 'NULL')."...") if $Mgd::DEBUG >= 2;
 

lib/MogileFS/Worker/Fsck.pm
@@ -71,7 +71,7 @@ sub work {
     my $sto         = Mgd::get_store();
     my $max_checked = 0;
 
-    every(1.0, sub {
+    every(2.0, sub {
         my $sleep_set = shift;
         $nowish = time();
         local $Mgd::nowish = $nowish;
@@ -87,12 +87,8 @@ sub work {
         }
 
         my $queue_todo = $self->queue_todo('fsck');
-        unless (@{$queue_todo}) {
-            $self->send_to_parent('worker_bored 50 fsck');
-            $self->read_from_parent(1);
-        } else {
-            $self->parent_ping;
-        }
+        # This counts the same as a $self->still_alive;
+        $self->send_to_parent('worker_bored 50 fsck');
 
         my @fids = ();
         while (my $todo = shift @{$queue_todo}) {

lib/MogileFS/Worker/JobMaster.pm
@@ -50,10 +50,12 @@ sub work {
         $self->send_to_parent("queue_depth all");
         my $sto = Mgd::get_store();
         $self->read_from_parent(1);
-        $self->_check_replicate_queues($sto);
-        $self->_check_delete_queues($sto);
-        $self->_check_fsck_queues($sto);
-        $self->_check_rebal_queues($sto);
+        my $active = 0;
+        $active += $self->_check_replicate_queues($sto);
+        $active += $self->_check_delete_queues($sto);
+        $active += $self->_check_fsck_queues($sto);
+        $active += $self->_check_rebal_queues($sto);
+        $_[0]->(0) if $active;
     });
 }
 
@@ -71,6 +73,7 @@ sub _check_delete_queues {
         $self->send_to_parent("queue_todo delete " .
             _eurl_encode_args($todo));
     }
+    return 1;
 }
 
 # NOTE: we only maintain one queue per worker, but we can easily
@@ -99,6 +102,7 @@ sub _check_replicate_queues {
         $self->send_to_parent("queue_todo replicate " .
             _eurl_encode_args($todo));
     }
+    return 1;
 }
 
 # FSCK is going to be a little odd... We still need a single "global"
@@ -127,6 +131,7 @@ sub _check_fsck_queues {
     for my $todo (@to_fsck) {
         $self->send_to_parent("queue_todo fsck " . _eurl_encode_args($todo));
     }
+    return 1;
 }
 
 sub _inject_fsck_queues {
@@ -179,6 +184,7 @@ sub _check_rebal_queues {
         $todo->{_type} = 'rebalance';
         $self->send_to_parent("queue_todo rebalance " . _eurl_encode_args($todo));
     }
+    return 1;
 }
 
 sub _inject_rebalance_queues {

lib/MogileFS/Worker/Reaper.pm
@@ -24,8 +24,6 @@ sub work {
     my $self = shift;
 
     every(5, sub {
-        $self->parent_ping;
-
         # get db and note we're starting a run
         debug("Reaper running; looking for dead devices");
 
@@ -40,7 +38,7 @@ sub work {
                 $all_empty{$devid} = 1;
                 next;
             }
-            $self->parent_ping;
+            $self->still_alive;
 
             foreach my $fid (@fids) {
                 # order is important here:

lib/MogileFS/Worker/Replicate.pm
@@ -37,7 +37,7 @@ sub work {
     # give the monitor job 15 seconds to give us an update
     my $warn_after = time() + 15;
 
-    every(2.0, sub {
+    every(1.0, sub {
         # replication doesn't go well if the monitor job hasn't actively started
         # marking things as being available
         unless ($self->monitor_has_run) {
@@ -49,22 +49,16 @@ sub work {
         $self->validate_dbh;
         my $dbh = $self->get_dbh or return 0;
         my $sto = Mgd::get_store();
-
         $self->send_to_parent("worker_bored 100 replicate rebalance");
-        # This is here on account of not being able to block on the parent :(
-        $self->read_from_parent(1);
-        # TODO: might need to sort types or create priority queues in the
-        # parent... would want "replicate" work to happen before rebalance.
+
         my $queue_todo  = $self->queue_todo('replicate');
         my $queue_todo2 = $self->queue_todo('rebalance');
         unless (@$queue_todo || @$queue_todo2) {
-            $self->parent_ping;
             return;
         }
 
         while (my $todo = shift @$queue_todo) {
             my $fid = $todo->{fid};
-            $self->still_alive;
             $self->replicate_using_torepl_table($todo);
         }
         while (my $todo = shift @$queue_todo2) {
@@ -81,11 +75,7 @@ sub work {
             # manually re-run rebalance to retry.
             $sto->delete_fid_from_file_to_queue($todo->{fid}, REBAL_QUEUE);
         }
-        # if replicators are otherwise idle, use them to make the world
-        # better, rebalancing things (if enabled), and draining devices (if
-        # any are marked drain)
-        #$self->rebalance_devices;
-        #$self->drain_devices;
+        $_[0]->(0); # don't sleep.
     });
 }
 

hrchu/bogomips-MogileFS-Server

commit 90889b8e56550827b207da67495699f00cc8537c

util: every() responds to ":wake_a" commands
Instead of blindly sleeping, workers select() on psock_fd in
order to respond to ":wake_a" commands.  This allows fsck
to start immediately when triggered.

This change does not increase wakeups during idle periods[1].

This fixes a regression introduced in
commit fa30b6aa44e870251079edcc158388549de57060
(Reduce unnecessary sleeping and parent traffic.)

[1] - verified by running strace on each worker

lib/MogileFS/Util.pm
@@ -84,7 +84,7 @@ sub every {
             next;
         }
 
-        Time::HiRes::sleep($sleep_for) if $sleep_for > 0;
+        wait_for_readability($psock_fd, $sleep_for) if $sleep_for > 0;
         #local $Mgd::POST_SLEEP_DEBUG = 1;
         # This calls read_from_parent. Workers used to needlessly call
         # parent_ping constantly.